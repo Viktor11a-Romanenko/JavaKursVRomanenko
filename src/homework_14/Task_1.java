c8941ad4b2f8b4e65be94ad9db50d8524138c814
ViktoriiaRomanenko <vika07romanenko13@gmail.com>
2024-09-30 09:26:27 +0200

    hw14 add

--- src/homework_14/Task_1.java
+++ src/homework_14/Task_1.java
@@ -1,4 +1,23 @@
 package homework_14;
 
 public class Task_1 {
-}
+    public static void main(String[] args) {
+        int[] ints = {1, 4, 9, 2, 8, 3};
+        int sum = sum(ints);
+        System.out.println("Сумма четных чисел: " + sum);
+
+    }//main
+
+    public static int sum(int[] ints) {
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] % 2 == 0) {
+                sum += ints[i];
+            }//if
+
+        }//for
+        return sum;
+
+    }
+
+}//end
