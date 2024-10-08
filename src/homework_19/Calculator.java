5ebadd8b2a8ce1a20c616357e745af1567c099c6
ViktoriiaRomanenko <vika07romanenko13@gmail.com>
2024-10-08 18:41:59 +0200

    homework_19 add

---
+++ src/homework_19/Calculator.java
@@ -0,0 +1,40 @@
+package homework_19;
+
+public class Calculator {
+
+    private double a;
+    private double b;
+    public static final double PI = 3.141519;
+
+
+    public static double plus(double a, double b) {
+        return a + b;
+
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        if (b == 0) {
+            System.out.println("error");
+        }
+        return a / b;
+    }
+
+    public static double length(final double PI, double r) {
+        return (2 * PI) * r;
+
+    }
+
+    public static double area(final double PI, double r) {
+        return PI * Math.pow(r, 2);
+
+
+    }
+}
