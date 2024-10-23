36fa72a52d349d88e04f9a1635aee166a88f04ac
ViktoriiaRomanenko <vika07romanenko13@gmail.com>
2024-10-23 15:00:17 +0200

    lesson_30 add

--- src/homework_27/PersonHwApp.java
+++ src/homework_27/PersonHwApp.java
@@ -3,41 +3,11 @@ package homework_27;
 public class PersonHwApp {
     public static void main(String[] args) {
 
-        /*
-        // Полезные методы класса Character
-        // Возвращает true если символ буква
-        Character.isAlphabetic('c');
-        // Возвращает true если символ цифра
-        Character.isDigit('1');
-        Character.isLetter('3');
-        Character.isLetterOrDigit('-');
-         */
 
         Person_HW person = new Person_HW("email", "password");
-        System.out.println(person);
-
         person.setEmail("test@mail.com");
         System.out.println(person.getEmail());
-
-        person.setEmail("a1@email.de");
-        System.out.println(person.getEmail());
-
-        System.out.println("\n========= \n");
-        person.setEmail("a1@ema@il.de");
-        person.setEmail("a1@email.d.e");
-        person.setEmail("a1@emailde");
-        person.setEmail("a1email.de");
-        System.out.println(person.getEmail());
-
-        System.out.println("\n============== \n");
-        person.setPassword("cQ1%dfa"); // длина меньше 8
         person.setPassword("cQ1%dfa_"); // валидный пароль
-        System.out.println("\n ================== ");
-        person.setPassword("cQ1%dfar"); //  валидный
-        person.setPassword("cQ1%dfa?"); // валидный
-        person.setPassword("cQ12dfa?"); // валидный
-        person.setPassword("cQ12dfa@паыпрыкры"); // валидный
-        System.out.println(person.getPassword());
 
 
     }
--- src/homework_27/Person_HW.java
+++ src/homework_27/Person_HW.java
@@ -56,19 +56,13 @@ public class Person_HW {
 
 
         // 3. После последней точки есть 2 или более символов
-        // test@fazx.com.ne.t
         int lastDotIndex = email.lastIndexOf('.');
         if (lastDotIndex + 2 >= email.length()) return false;
 
         // 4.  Алфавит, цифры, '-', '_', '.', '@'
-        /*
-        Я беру каждый символ. Проверяю, что он не является "запрещенным"
-        И если нахожу не подходящий символ - возвращаю false
-         */
         for (int i = 0; i < email.length(); i++) {
             char ch = email.charAt(i);
 
-            // Если символ удовлетворяет одному из условий на "правильность"
             boolean isPass = (Character.isAlphabetic(ch) ||
                     Character.isDigit(ch) ||
                     ch == '-' ||
@@ -76,26 +70,19 @@ public class Person_HW {
                     ch == '.' ||
                     ch == '@');
 
-            // Если любой символ НЕ подходящий, сразу возвращаем false
+
             if (!isPass) return false;
 
-            /*
-            Равнозначные выражения.
-            if (!Character.isAlphabetic(ch) && !Character.isDigit(ch) && ch != '-' && ch != '_' && ch != '.' && ch != '@') return false;
-            if (!(Character.isAlphabetic(ch) || Character.isDigit(ch) || ch == '-' || ch == '_' || ch == '.' || ch == '@')) return false;
-             */
         }
 
-        // 5. До собаки должен быть хотя бы 1 символ == собака не первая в строке. Т.е. ее индекс не равен 0
+        // 5. До собаки должен быть хотя бы 1 символ
         if (indexAt == 0) return false;
 
         // 6. Первый символ - должна быть буква
-        // Если 0-й символ НЕ является буквой, то email не подходит = return false;
         char firstChar = email.charAt(0);
         if (!Character.isAlphabetic(firstChar)) return false;
 
 
-        // Все проверки пройдены. email подходит.
         return true;
 
     }
@@ -112,26 +99,23 @@ public class Person_HW {
         boolean isLowerCase = false;
         boolean isSpecialSymbol = false;
 
-        // альтернативный способ объявление переменных
-        boolean[] result = new boolean[4]; // false, false, false, false
+        boolean[] result = new boolean[4];
 
         String symbols = "!%$@&*()[].,-";
 
-        // Перебираю символы
         for (int i = 0; i < password.length(); i++) {
             char ch = password.charAt(i);
 
-            if (Character.isDigit(ch)) isDigit = true; // res[0] = true
-            if (Character.isUpperCase(ch)) isUpperCase = true; // res[1] = true
-            if (Character.isLowerCase(ch)) isLowerCase = true; //  res[2] = true
+            if (Character.isDigit(ch)) isDigit = true;
+            if (Character.isUpperCase(ch)) isUpperCase = true;
+            if (Character.isLowerCase(ch)) isLowerCase = true;
             if (symbols.indexOf(ch) >= 0) isSpecialSymbol = true;
-            // if (symbols.contains(String.valueOf(ch))) isSpecialSymbol = true;
+
 
         }
 
         System.out.printf("%s | %s | %s | %s\n", isDigit, isUpperCase, isLowerCase, isSpecialSymbol);
 
-        // Если хотя бы в одной переменной останется значение false, то весь пароль НЕ будет признан валидным = (признан не валидным)
         return isDigit && isUpperCase && isLowerCase && isSpecialSymbol;
 
     }
